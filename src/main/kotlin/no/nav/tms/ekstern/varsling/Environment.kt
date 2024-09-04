5ba260969455fbe50ae51539e52b17f968f7cb01
Christopher Santana <christopher.santana@nav.no>
2024-09-04 14:49:59 +0200

    Legg ut i prod.

--- src/main/kotlin/no/nav/tms/ekstern/varsling/Environment.kt
+++ src/main/kotlin/no/nav/tms/ekstern/varsling/Environment.kt
@@ -1,9 +1,8 @@
 package no.nav.tms.ekstern.varsling
 
-import no.nav.tms.common.util.config.BooleanEnvVar.getEnvVarAsBoolean
 import no.nav.tms.common.util.config.StringEnvVar.getEnvVar
 
 data class Environment(
     val varselTopic: String = "min-side.brukervarsel-v1",
-    val groupId: String = "ekstern-varsling-02"
+    val groupId: String = getEnvVar("KAFKA_GROUP_ID")
 )
