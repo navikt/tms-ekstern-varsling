fe65ea3a2ae94c5f91ce2076287691a05025115e
Christopher Santana <christopher.santana@nav.no>
2024-09-09 11:39:54 +0200

    Test.

--- src/main/kotlin/no/nav/tms/ekstern/varsling/setup/Flyway.kt
+++ src/main/kotlin/no/nav/tms/ekstern/varsling/setup/Flyway.kt
@@ -1,21 +1,27 @@
 package no.nav.tms.ekstern.varsling.setup
 
+import io.github.oshai.kotlinlogging.KotlinLogging
 import org.flywaydb.core.Flyway
 import org.flywaydb.core.api.configuration.FluentConfiguration
 
 object Flyway {
 
+    val log = KotlinLogging.logger {}
+
     fun runFlywayMigrations() {
         val flyway = configure().load()
         flyway.migrate()
+
+        log.info { "Flyway complete" }
     }
 
     private fun configure(): FluentConfiguration {
         val configBuilder = Flyway.configure()
-            .validateMigrationNaming(true)
             .connectRetries(5)
         val dataSource = PostgresDatabase.hikariFromLocalDb()
         configBuilder.dataSource(dataSource)
+
+        log.info { "Prepared flyway" }
 
         return configBuilder
     }
