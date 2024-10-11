0909d8d36dd2c22c415c165f8a8232c442b7a304
Christopher Santana <christopher.santana@nav.no>
2024-10-11 12:54:45 +0200

    Debug.

--- buildSrc/src/main/kotlin/default/dependencies.kt
+++ buildSrc/src/main/kotlin/default/dependencies.kt
@@ -228,7 +228,7 @@ interface TmsCommonLibDefaults: DependencyGroup {
 
 interface TmsKafkaToolsDefaults: DependencyGroup {
     override val groupId get() = "no.nav.tms.kafka"
-    override val version get() = "1.5.0-onReady-2"
+    override val version get() = "1.5.0-onReady-3"
 
     val kafkaApplication get() = dependency("kafka-application")
 }
