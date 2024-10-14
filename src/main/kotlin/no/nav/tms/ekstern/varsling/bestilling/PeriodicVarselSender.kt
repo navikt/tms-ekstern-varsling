711e53d5a7ba85e494fa155cae4fbf137a4600e1
Christopher Santana <christopher.santana@nav.no>
2024-10-14 12:33:19 +0200

    Debug.

--- src/main/kotlin/no/nav/tms/ekstern/varsling/bestilling/PeriodicVarselSender.kt
+++ src/main/kotlin/no/nav/tms/ekstern/varsling/bestilling/PeriodicVarselSender.kt
@@ -25,11 +25,18 @@ class PeriodicVarselSender(
 
     private val log = KotlinLogging.logger {}
 
+    private var count = -1
+
     override val job = initializeJob {
         if (leaderElection.isLeader()) {
             repository
                 .nextInVarselQueue(gracePeriod)
                 .forEach(::processRequest)
+        } else {
+            count = (count + 1) % 60
+            if (count == 0) {
+                log.info { "Was not leader" }
+            }
         }
     }
 
